2011e033094e80f0cceaaa15c69468c3efa625e9
francis.l <francis.l@binance.com>
2022-12-21 10:20:05 +0800

    Initial Commit

---
+++ AppleFoodPin/AppleFoodPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFoodPinApp.swift
+//  AppleFoodPin
+//
+//  Created by user on 2022/12/21.
+//
+
+import SwiftUI
+
+@main
+struct AppleFoodPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleFoodPin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppleFoodPin
+//
+//  Created by user on 2022/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
