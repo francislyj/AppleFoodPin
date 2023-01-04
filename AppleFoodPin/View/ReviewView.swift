45d121f21dae2c201e7ca13dc7123397e8cd5cda
francis.liu <francislyj@gmail.com>
2023-01-04 19:49:25 +0800

    1

--- AppleFoodPin/View/ReviewView.swift
+++ AppleFoodPin/View/ReviewView.swift
@@ -8,9 +8,21 @@
 import SwiftUI
 
 struct ReviewView: View {
+    
+    var restaurant: Restaurant
+    
     var body: some View {
         ZStack {
-            Color.black.ignoresSafeArea()
+            Image(restaurant.image)
+                .resizable()
+                .scaledToFill()
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .ignoresSafeArea()
+            
+            Color.black
+                .opacity(0.6)
+                .background(.ultraThinMaterial)
+                .ignoresSafeArea()
             
             HStack {
                 Spacer()
@@ -46,6 +58,6 @@ struct ReviewView: View {
 
 struct ReviewView_Previews: PreviewProvider {
     static var previews: some View {
-        ReviewView()
+        ReviewView(restaurant: Restaurant(name: "Royal Oak", type: "British", location: "2 Regency Street London SW1P 4BZ United Kingdom", phone: "343-988834", description: "Specialise in great pub food. Established in 1872, we have local and world lagers, craft beer and a selection of wine and spirits for people to enjoy. Don't forget to try our range of Young's Ales and Fish and Chips.", image: "royaloak", isFavorite: true))
     }
 }
