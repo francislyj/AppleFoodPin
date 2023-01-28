45960bd93e53ad6342f8f53af3576515d3b2d25a
francis.liu <francislyj@gmail.com>
2023-01-28 16:15:08 +0800

    1

--- AppleFoodPin/AppleFoodPinApp.swift
+++ AppleFoodPin/AppleFoodPinApp.swift
@@ -10,6 +10,8 @@ import SwiftUI
 @main
 struct AppleFoodPinApp: App {
     
+    @Environment(\.scenePhase) var scenePhase
+    
     let persistenceController = PersistenceController.shared
     
     init() {
@@ -28,12 +30,38 @@ struct AppleFoodPinApp: App {
         UINavigationBar.appearance().standardAppearance = navBarAppearance
         UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
         UINavigationBar.appearance().compactAppearance = navBarAppearance
+        
+        
+       
+    }
+    
+    
+    func createQuickActions() {
+        if let bundleIdentifier = Bundle.main.bundleIdentifier {
+            let shortcutItem1 = UIApplicationShortcutItem(type: "\(bundleIdentifier).OpenFavorites", localizedTitle: "Show Favorites", localizedSubtitle: nil, icon: UIApplicationShortcutIcon(systemImageName: "tag"), userInfo: nil)
+            let shortcutItem2 = UIApplicationShortcutItem(type: "\(bundleIdentifier).OpenDiscover", localizedTitle: "Discover Restaurants", localizedSubtitle: nil, icon: UIApplicationShortcutIcon(systemImageName: "eyes"), userInfo: nil)
+            let shortcutItem3 = UIApplicationShortcutItem(type: "\(bundleIdentifier).NewRestaurant", localizedTitle: "New Restaurant", localizedSubtitle: nil, icon: UIApplicationShortcutIcon(type: .add), userInfo: nil)
+            UIApplication.shared.shortcutItems = [shortcutItem1, shortcutItem2, shortcutItem3]
+        }
     }
     
     var body: some Scene {
         WindowGroup {
             MainView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+        .onChange(of: scenePhase) { (phase) in
+            switch phase {
+            case .active:
+                print("active")
+            case .inactive:
+                print("inactive")
+            case .background:
+                createQuickActions()
+            @unknown default:
+                print("default scene phase")
+            }
+            
         }
     }
 }
